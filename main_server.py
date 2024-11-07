37b8a59edb320a656490d165628c58d80c8ff14d
Adam Topolski <a.topolski.at@gmail.com>
2024-11-07 22:28:03 +0100

    Somewhat works

--- main_server.py
+++ main_server.py
@@ -6,25 +6,33 @@ import ray
 from ray import serve
 from starlette.requests import Request
 
+from context_tracker import ContextTracker
+
+
+# serve run main_server:my_model_deployment
+
 
 @serve.deployment(ray_actor_options={"num_cpus": 1, "num_gpus": 1})
 class MyModelDeployment:
     def __init__(self):
-        self.model = whisper.load_model("small").cuda()
+        self.ctx_tracker = ContextTracker.remote()
+        self.model = whisper.load_model("turbo").cuda()
         print(self.model.device)
 
     async def __call__(self, request: Request) -> Dict:
-
+        context = await self.ctx_tracker.get_tokens.remote()
         request = await request.body()
         audio = np.frombuffer(request, np.float32)
         audio = whisper.pad_or_trim(audio)
-        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
-        options = whisper.DecodingOptions()
+        mel = whisper.log_mel_spectrogram(audio, n_mels=128).to(self.model.device)
+        options = whisper.DecodingOptions(prompt=context, prefix=context[-20:])
         result = self.model.decode(mel, options)
+        if result.no_speech_prob > 0.4:
+            return "[EMPTY]"
+        self.ctx_tracker.add_tokens.remote(context)
         return result.text
 
 
 ray.init()
 
 my_model_deployment = MyModelDeployment.bind()
-
--- test_client.py
+++ test_client.py
@@ -1,8 +1,15 @@
 import requests
-import whisper
+import soundcard as sc
 
-audio = whisper.load_audio("sample-4.mp3")
-print(audio.shape)
-resp = requests.post("http://127.0.0.1:8000/", data=audio.tobytes())
-print(resp.text)
-whisper.transcribe()
+fs = 16000  # THIS
+print(sc.all_microphones())
+default_mic = sc.all_microphones(exclude_monitors=False)[1]#sc.default_microphone()
+print(default_mic)
+
+data = None
+with default_mic.recorder(fs, channels=1, blocksize=fs*5) as rec:
+    while True:
+        data = rec.record(int(3 * fs)).flatten()
+        resp = requests.post("http://127.0.0.1:8000/", data=data.tobytes())
+        print(resp.text)
+
